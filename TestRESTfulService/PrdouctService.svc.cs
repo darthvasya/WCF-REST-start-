d10b208d9996c8dd9ef102ca1c99d95b3872168c
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-05-19 22:07:16 +0300

    init commit

---
+++ TestRESTfulService/PrdouctService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TestRESTfulService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PrdouctService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PrdouctService.svc or PrdouctService.svc.cs at the Solution Explorer and start debugging.
+    public class PrdouctService : IPrdouctService
+    {
+        public void DoWork()
+        {
+        }
+
+
+    }
+}
---
+++ TestRESTfulService/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TestRESTfulService
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string CategoryName { get; set; }
+
+        [DataMember]
+        public int Price { get; set; }
+    }
+
+    public partial class Products
+    {
+        private static readonly Products _instance = new Products();
+
+        private Products() { }
+
+        public static Products Instance
+        {
+            get { return _instance; }
+        }
+
+       // public List<Product> ProductList
+    }
+}
